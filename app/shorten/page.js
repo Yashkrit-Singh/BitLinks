a613e3f508b32472387b418d599d8d7574123d4f
Yashkrit Singh <139995167+Yashkrit-Singh@users.noreply.github.com>
2025-07-16 02:03:15 +0530

    updated

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -27,7 +27,7 @@ const Shorten = () => {
     fetch("/api/generate", requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        setGenerated(`${process.env.HOST}/${shorturl}`);
+        setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`);
         seturl("");
         setshorturl("");
         console.log(result);
